daf6f17a84d1f74f6c9fc374ef155639ab6aee6b
Matteo Pagani <matteo.pagani@microsoft.com>
2023-05-14 14:53:41 +0200

    Add project files.

---
+++ BlazorConf2023/Config.cs
@@ -0,0 +1,13 @@
+using Microsoft.TeamsFx.Configuration;
+
+namespace BlazorConf2023
+{
+    public class ConfigOptions
+    {
+        public TeamsFxOptions TeamsFx { get; set; }
+    }
+    public class TeamsFxOptions
+    {
+        public AuthenticationOptions Authentication { get; set; }
+    }
+}
---
+++ BlazorConf2023/Models/DriveFile.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorConf2023.Models
+{
+    public class DriveFile
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
